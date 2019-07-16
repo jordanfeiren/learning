84ba3bc03324c136f57239f63a9dcfa19b76f2e3
machao <jordanfeiren@163.com>
2019-07-16 21:09:30 +0800

    Merge branch 'master' of https://github.com/jordanfeiren/learning
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("so Wor少时诵诗书所所所所所所ld!");
-        System.out.println("cccccssccso World!");
+        System.out.println("cccccsssssccso World!");
     }
 }
